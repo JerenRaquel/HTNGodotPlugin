fe882836355d98b31d5a862f403c77dea1da508d
Jeren Raquel <jeraquel101@gmail.com>
2024-06-03 22:26:31 -0700

    button context switching

--- addons/HTNDomainManager/PluginSystem/ConditionEditor/ConditionLine/ConditionLineManager.gd
+++ addons/HTNDomainManager/PluginSystem/ConditionEditor/ConditionLine/ConditionLineManager.gd
@@ -10,6 +10,14 @@ extends VBoxContainer
 #4:	# Less Than or Equal To
 #5:	# Range
 
+## Single Type IDs
+#0:	# Boolean
+#1:	# Int
+#2:	# Float
+#3:	# String
+#4:	# Vector2
+#5:	# Vector3
+
 # Options
 @onready var range_type_option: OptionButton = %RangeTypeOption
 @onready var single_type_option: OptionButton = %SingleTypeOption
@@ -41,10 +49,22 @@ func initialize(data: Dictionary = {}) -> void:
 
 func _show_based_on_compare_type(idx: int) -> void:
 	_compare_type = idx
+	_on_data_container_hidden()
 	match idx:
-		0, 1, 2, 3, 4:	# Less Than
+		0, 1, 3, 4:
 			range_type_option.hide()
 			single_type_option.show()
+			single_type_option.set_item_disabled(0, true)
+			single_type_option.set_item_disabled(3, true)
+			single_type_option.set_item_disabled(4, true)
+			single_type_option.set_item_disabled(5, true)
+			if single_type_option.selected != 1 and single_type_option.selected != 2:
+				single_type_option.select(1)
+			_show_based_on_single_type_ID(single_type_option.selected)
+		2:	# Equals Only
+			range_type_option.hide()
+			single_type_option.show()
+			_show_based_on_single_type_ID(single_type_option.selected)
 		5:	# Range
 			range_type_option.show()
 			single_type_option.hide()
@@ -52,6 +72,28 @@ func _show_based_on_compare_type(idx: int) -> void:
 			_show_spin_box(1, "End", true, true)
 			range_separator.show()
 			data_container.show()
+
+func _show_based_on_single_type_ID(idx: int) -> void:
+	if not data_container.visible: data_container.show()
+	_on_data_container_hidden()
+	match idx:
+		0:	# Boolean
+			is_true_toggle.show()
+		1:	# Int
+			_show_spin_box(0, "Value: ", true, false)
+		3:	# String
+			string_value.show()
+		_:	# Float Based Data Types
+			var text: String = "Value: "
+			if idx == 5:	# Vector3
+				_show_spin_box(2, "Z: ", false, false)
+				text = "X: "
+			if idx >= 4:	# Vector2
+				_show_spin_box(1, "Y: ", false, false)
+				text = "X: "
+
+			_show_spin_box(0, text, false, false)
+
 
 func _show_spin_box(component_idx: int, label_text: String, is_rounded: bool, enable_checkbox: bool) -> void:
 	var spin_box: SpinBox
@@ -88,7 +130,7 @@ func _on_range_type_option_item_selected(index: int) -> void:
 	_show_spin_box(1, "End", true if index == 0 else false, true)
 
 func _on_single_type_option_item_selected(index: int) -> void:
-	pass # Replace with function body.
+	_show_based_on_single_type_ID(index)
 
 func _on_data_container_hidden() -> void:
 	is_true_toggle.hide()
@@ -102,3 +144,7 @@ func _on_data_container_hidden() -> void:
 	x_check_box.hide()
 	y_check_box.hide()
 	range_separator.hide()
+	single_type_option.set_item_disabled(0, false)
+	single_type_option.set_item_disabled(3, false)
+	single_type_option.set_item_disabled(4, false)
+	single_type_option.set_item_disabled(5, false)
